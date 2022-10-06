30a30708e13f469950d501a6eadb99a7f1b59a63
Fabio Benedetti <fabio.benedetti@vercel.com>
2022-10-06 13:57:00 +0200

    test

--- pages/api/webhooks.tsx
+++ pages/api/webhooks.tsx
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const TOKEN = "aDfz6ZwDnHzgimgM3ehOSSi7";
 
-const postMessage = ({
+const postMessage = async ({
   deploymentId,
   webhookType,
   level,
@@ -13,86 +13,93 @@ const postMessage = ({
   payload?: any;
   level: "info" | "error" | "debug";
 }) => {
-  fetch("https://cloud.axiom.co/api/v1/datasets/vercel/ingest", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer xaat-91ca3499-12a8-406e-96b5-5f17348e985c",
-    },
-    body: JSON.stringify({
-      time: new Date().toISOString(),
-      data: { payload },
-      level,
-      tags: { deploymentId, webhookType },
-    }),
-  });
+  const d = await fetch(
+    "https://cloud.axiom.co/api/v1/datasets/vercel/ingest",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer xaat-91ca3499-12a8-406e-96b5-5f17348e985c",
+      },
+      body: JSON.stringify({
+        time: new Date().toISOString(),
+        data: { payload },
+        level,
+        tags: { deploymentId, webhookType },
+      }),
+    }
+  ).then((r) => r.json());
+  console.log(d);
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.body.type) {
-    case "deployment":
-      const check = await fetch(
-        `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks`,
-        {
-          body: JSON.stringify({
-            blocking: true,
-            name: "Fake Check",
-          }),
-          headers: {
-            Authorization: `Bearer ${TOKEN}`,
-            "Content-Type": "application/json",
-          },
-          method: "post",
-        }
-      ).then((r) => r.json());
+  try {
+    switch (req.body.type) {
+      case "deployment":
+        const check = await fetch(
+          `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks`,
+          {
+            body: JSON.stringify({
+              blocking: true,
+              name: "Fake Check",
+            }),
+            headers: {
+              Authorization: `Bearer ${TOKEN}`,
+              "Content-Type": "application/json",
+            },
+            method: "post",
+          }
+        ).then((r) => r.json());
 
-      postMessage({
-        deploymentId: req.body.payload.deployment.id,
-        webhookType: req.body.type,
-        payload: check,
-        level: "debug",
-      });
-      break;
-    case "deployment-prepared":
-      const data = await fetch(
-        `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks`,
-        {
-          headers: {
-            Authorization: `Bearer ${TOKEN}`,
-          },
-          method: "get",
-        }
-      ).then((r) => r.json());
+        await postMessage({
+          deploymentId: req.body.payload.deployment.id,
+          webhookType: req.body.type,
+          payload: check,
+          level: "debug",
+        });
+        break;
+      case "deployment-prepared":
+        const data = await fetch(
+          `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks`,
+          {
+            headers: {
+              Authorization: `Bearer ${TOKEN}`,
+            },
+            method: "get",
+          }
+        ).then((r) => r.json());
 
-      const result = await fetch(
-        `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks/${data.checks[0].id}`,
-        {
-          body: JSON.stringify({
-            conclusion: "succeeded",
-            status: "completed",
-          }),
-          headers: {
-            Authorization: `Bearer ${TOKEN}`,
-            "Content-Type": "application/json",
-          },
-          method: "patch",
-        }
-      ).then((r) => r.json());
+        const result = await fetch(
+          `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks/${data.checks[0].id}`,
+          {
+            body: JSON.stringify({
+              conclusion: "succeeded",
+              status: "completed",
+            }),
+            headers: {
+              Authorization: `Bearer ${TOKEN}`,
+              "Content-Type": "application/json",
+            },
+            method: "patch",
+          }
+        ).then((r) => r.json());
 
-      postMessage({
-        deploymentId: req.body.payload.deployment.id,
-        webhookType: req.body.type,
-        payload: { checks: data, updateCheck: result },
-        level: "debug",
-      });
-    default:
-      postMessage({
-        deploymentId: req.body.payload.deployment.id,
-        webhookType: req.body.type,
-        payload: null,
-        level: "debug",
-      });
+        await postMessage({
+          deploymentId: req.body.payload.deployment.id,
+          webhookType: req.body.type,
+          payload: { checks: data, updateCheck: result },
+          level: "debug",
+        });
+      default:
+        await postMessage({
+          deploymentId: req.body.payload.deployment.id,
+          webhookType: req.body.type,
+          payload: null,
+          level: "debug",
+        });
+    }
+  } catch (e) {
+    console.log(e);
   }
-
   res.json({ done: true });
 };
