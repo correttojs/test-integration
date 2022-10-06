6e600e20a30b1b2d1874500c95ac4aafff16ad30
Fabio Benedetti <fabio.benedetti@vercel.com>
2022-10-06 10:28:03 +0200

    chore: test

--- pages/api/webhooks.tsx
+++ pages/api/webhooks.tsx
@@ -23,32 +23,32 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     ).then((r) => r.json());
     console.log("Check", check);
   }
-  if (req.body.type === "deployment-prepared") {
-    const data = await fetch(
-      `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks`,
-      {
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-        },
-        method: "get",
-      }
-    ).then((r) => r.json());
-    console.log("Checks", data);
-    console.log("CheckId", data.checks[0].id);
-    await fetch(
-      `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks/${data.checks[0].id}`,
-      {
-        body: JSON.stringify({
-          conclusion: "succeeded",
-          status: "completed",
-        }),
-        headers: {
-          Authorization: `Bearer ${TOKEN}`,
-          "Content-Type": "application/json",
-        },
-        method: "patch",
-      }
-    );
-  }
+  // if (req.body.type === "deployment-prepared") {
+  //   const data = await fetch(
+  //     `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks`,
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${TOKEN}`,
+  //       },
+  //       method: "get",
+  //     }
+  //   ).then((r) => r.json());
+  //   console.log("Checks", data);
+  //   console.log("CheckId", data.checks[0].id);
+  //   await fetch(
+  //     `https://api.vercel.com/v1/deployments/${req.body.payload.deployment.id}/checks/${data.checks[0].id}`,
+  //     {
+  //       body: JSON.stringify({
+  //         conclusion: "succeeded",
+  //         status: "completed",
+  //       }),
+  //       headers: {
+  //         Authorization: `Bearer ${TOKEN}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //       method: "patch",
+  //     }
+  //   );
+  // }
   res.json({ done: true });
 };
